c932c88606a4d1b3fbfc6b282a7e6fbce45e7f93
Martin Schibel <mshd94@gmail.com>
2020-06-05 11:01:47 +0200

    add h1 #2

--- tests/test.php
+++ tests/test.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 //use libreja/Sru;
+echo '<h1>Test searching for book:</h1><br />';
 use Libreja\SruCatalog;
 $sruCatalog = new SruCatalog\CatalogMain();
 $sruCatalog->service = "dnb";
