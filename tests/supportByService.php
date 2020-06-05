4b957a311b94be1d82bc7d09dbb910ca80fd1c0d
Martin Schibel <mshd94@gmail.com>
2020-06-05 11:32:25 +0200

    add list services supports #3

---
+++ tests/supportByService.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
+use Libreja\SruCatalog;
+$sruCatalog = new SruCatalog\ServicesList();
+
+$table = [];
+foreach($sruCatalog->supportedKeys as $key=>$val){
+  $table[$key] = [];
+}
+$html = "| . |";
+foreach($sruCatalog->getServices() as $serviceKey=>$serviceValue){
+  $html .= " ".$serviceKey." |";
+  foreach($table as $searchKey => $searchValue){
+    array_push($table[$searchKey],array_key_exists($searchKey,$serviceValue["search"]));
+  }
+}
+
+foreach($table as $key => $value){
+  $html .= "\n| ".$key." | ";
+  foreach($value as $exists){
+    $html .= " ".($exists?"x":"")." |";
+  }
+}
+echo $html;
